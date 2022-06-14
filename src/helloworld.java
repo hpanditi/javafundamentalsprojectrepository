ec7feef63c8b24ebaa10a7619e15305e3c16a668
Hemanth Panditi <panditih@mail.gvsu.edu>
2022-06-14 00:01:29 -0400

    Update helloworld.java

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,5 +3,7 @@ public class helloworld {
     public static void main(String[] args) {
 
         System.out.println("Hello World! Greetings!");
+
+        System.out.println("Earth is a beautiful planet");
     }
 }
